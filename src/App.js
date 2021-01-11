60375b4060968dab20930ee16b8efd718ae4c8bb
Sanjana Udar <sanjana.udar@gmail.com>
2021-01-11 14:57:28 -0500

    deleting routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Forms from './components/Forms';
-import Fail from './components/Fail';
-import Success from './components/Success';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Forms}/>
-        <Route path="/success" exact component={Success}/>
-        <Route path="/fail" exact component={Fail}/>
-        <Route path="/" render={() => <div>404</div>}/>
-      </Switch>
-    </BrowserRouter>
+    <Forms />
   );
 }
 
